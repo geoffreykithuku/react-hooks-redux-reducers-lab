2fead0781b5b3a62a048be0ce71ded3526388d91
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-05-09 16:26:45 +0300

    create reducers

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,22 @@
+let state = {
+  friends: [],
+};
+
 export function manageFriends(state, action) {
-  // your code here
+  const { type, payload } = action;
+  switch (type) {
+    case "friends/add":
+      return { ...state, friends: [...state.friends, payload] };
+    case "friends/remove":
+      let newArr = [...state.friends];
+      for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i].id === payload) {
+          newArr.splice(i, 1);
+          break;
+        }
+      }
+      return { ...state, friends: newArr };
+    default:
+      return state;
+  }
 }
